756d99009837d6f8eeda55c0d40f1edc49483273
Linus <linus@ronaldjacob.de>
2017-12-30 20:36:16 +0100

    Created Xhr request

--- index.js
+++ index.js
@@ -5,7 +5,26 @@ let top3Req = new XMLHttpRequest();
 top3Req.open("GET", "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=3");
 top3Req.onreadystatechange = function () {
     if (this.readyState === 4 && this.status === 200) {
-        console.log(this.responseText);
+        let data = JSON.parse(this.responseText);
+        let boxes = [ document.querySelector(".box-big"), document.querySelector(".box-medium"), document.querySelector(".box-small")];
+        for ( let i=0; i<data.length; i++) {
+            addDataToBox(data[i], boxes[i])
+        };
+        console.log(data);
     }
 };
 top3Req.send();
+
+function addDataToBox (obj, box) {
+    
+    box.innerHTML =
+    `<h3 class="box-title">${obj.name}</h3>
+    <ul class="box-data">
+        <li>Symbol: ${obj.symbol}</li>
+        <li>Rank: ${obj.rank}</li>
+        <li>Price $: ${Number(obj.price_usd).toFixed(2)}</li>
+        <li>Price €: ${Number(obj.price_eur).toFixed(2)}</li>
+        <li>Supply: ${obj.total_supply}</li>
+    </ul>
+    `
+}
