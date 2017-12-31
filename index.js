cbe9a7a40162d7ea70df09e0c48f92092682f6a1
Linus <linus@ronaldjacob.de>
2017-12-31 18:04:48 +0100

    Added Request for custom currency

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const coinbinUrl = "https://coinbin.org/";
-// API request objects
+
 let top3Req = new XMLHttpRequest();
 
 top3Req.open("GET", "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=3");
@@ -27,4 +26,25 @@ function addDataToBox (obj, box) {
         <li>Supply: ${obj.total_supply}</li>
     </ul>
     `
+}
+function searchCurrency () {
+    let searchValue = document.getElementById("currencySearch").value.toLowerCase(),
+    currencyReq = new XMLHttpRequest();
+
+    currencyReq.open("GET", `https://api.coinmarketcap.com/v1/ticker/${searchValue}/?convert=EUR`);
+    currencyReq.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            let data = JSON.parse(this.responseText),
+            newDiv = document.createElement("div");
+            newDiv.className = "grid-box box-medium";
+            document.getElementById("searchresult").appendChild(newDiv);
+            addDataToBox(data[0], newDiv);
+        } else if (this.readyState === 4) {
+            document.getElementById("searchresult").innerHTML = 
+            "<p id='error-message'>Something went wrong. Make sure you spelled the name of the currency correctly.</p>"
+        }
+    }
+
+    currencyReq.send();
+    console.log(searchValue);
 }
