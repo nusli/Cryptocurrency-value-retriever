07ddab78fbdb056f2c36b7c10bf9148d965615cd
Linus <linus@ronaldjacob.de>
2018-01-02 21:01:16 +0100

    Experimented with cookies

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 let top3Req = new XMLHttpRequest();
+document.cookie = "test=test";
+alert(document.cookie);
 
 top3Req.open("GET", "https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=3");
 top3Req.onreadystatechange = function () {
@@ -27,7 +29,39 @@ function addDataToBox (obj, box) {
     </ul>
     `
 }
+
 function searchCurrency () {
+    // cookie functions to limit api requests
+    function setCountCookie () {
+        // prepare everything for the cookie
+        let APICount = getAPICount(),
+        d = new Date();
+        d.setTime(d.getTime() + (5*60*1000)); // set time to 1min in the future
+        let expires = "expires=" + d.toGMTString();
+
+        if (APICount > 0 ) { //Increase count by 1
+            document.cookie = `apiRequests=${Number(APICount)++}; ${expires}; path=/`;
+        } else { // set new cookie, count = 1
+            document.cookie = "apiRequests=1;" + expires + ";path=/";
+        }
+    }
+    function getAPICount () {
+        let allCookies = document.cookie.split(";"),
+        cookieName = "apiRequests";
+        allCookies.forEach(cookiePair => {
+            if (cookiePair.search(cookieName) >= 0) {
+                alert(cookiePair);
+                let eqIndex = cookiePair.indexOf("=");
+                return Number(cookiePair.slice(eqIndex)); 
+            }
+        });
+        return 0;
+    }
+
+    if (getAPICount >= 2) {
+        document.getElementById("searchresult").innerHTML = 
+            "<p id='error-message'>Too many requests. You cannot send more than 2 requests per minute.</p>";
+    }
     let searchValue = document.getElementById("currencySearch").value.toLowerCase(),
     currencyReq = new XMLHttpRequest();
 
@@ -36,12 +70,13 @@ function searchCurrency () {
         if (this.readyState === 4 && this.status === 200) {
             let data = JSON.parse(this.responseText),
             newDiv = document.createElement("div");
-            newDiv.className = "grid-box box-medium";
+            newDiv.className = "grid-box box-medium box-orange";
             document.getElementById("searchresult").appendChild(newDiv);
             addDataToBox(data[0], newDiv);
+            setCountCookie();
         } else if (this.readyState === 4) {
             document.getElementById("searchresult").innerHTML = 
-            "<p id='error-message'>Something went wrong. Make sure you spelled the name of the currency correctly.</p>"
+            "<p id='error-message'>Something went wrong. Make sure you spelled the name of the currency correctly.</p>";
         }
     }
 
